bba1d3b913ee8de1b235849eced871fa5b876ea1
pitukemaster@gmail.com <pitukemaster@gmail.com@0f6ebd93-bc43-3e5c-799d-1a7754f0039d>
2012-11-05 17:43:09 +0000

    Matej: upravena datova struktura na modeloch

---
+++ trunk/application/models/deleter.php
@@ -0,0 +1,9 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Deleter extends MY_Model
+{
+    
+}
+
+/* End of file deleter.php */
+/* Location: ./application/models/deleter.php */
