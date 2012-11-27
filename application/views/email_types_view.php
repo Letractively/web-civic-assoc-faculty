96875e6493a7caded80104e90df06ad005004c60
pitukemaster <pitukemaster@gmail.com>
2012-11-27 17:44:47 +0000

    Mino->email_types_view.php     ->project_categories_view.php

--- application/views/email_types_view.php
+++ application/views/email_types_view.php
@@ -1,7 +1,21 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+    $this->load->library('grid');
+    
+    $grid = new Grid();
+    
+    //array_debug($this->selecter->get_events(1));
+    
+    $grid->bind($this->selecter->get_email_types(), 'email_id');
+    
+//    $grid->header('degree_name')->component->type = "combobox";
+//    $grid->header('degree_name')->component->bind( $this->selecter->get_email_types(), "email_id", "email_type");
+    
+    $grid->add_url = "{$this->router->class}/add";
+    $grid->edit_url = "{$this->router->class}/edit";
+    $grid->remove_url = "{$this->router->class}/delete";
+    
+    $grid->header('email_id')->editable = false;
+    
+	
+    $grid->display();
 ?>
--- application/views/project_categories_view.php
+++ application/views/project_categories_view.php
@@ -1,7 +1,18 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+    $this->load->library('grid');
+    
+    $grid = new Grid();
+    
+    //array_debug($this->selecter->get_events(1));
+    
+    $grid->bind($this->selecter->get_project_categories(), 'project_categories_id');
+    
+    $grid->add_url = "{$this->router->class}/add";
+    $grid->edit_url = "{$this->router->class}/edit";
+    $grid->remove_url = "{$this->router->class}/delete";
+    
+    $grid->header('project_categories_id')->editable = false;
+    
+	
+    $grid->display();
 ?>
