eb65575af58ceb5f9a3eabb168356d25208e27ec
pitukemaster@gmail.com <pitukemaster@gmail.com@0f6ebd93-bc43-3e5c-799d-1a7754f0039d>
2012-11-28 14:28:53 +0000

    Mino: nejake views

--- trunk/application/views/posts_detail_view.php
+++ trunk/application/views/posts_detail_view.php
@@ -1,7 +1,24 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+    $obj = $this->selecter->get_post_detail(1);
+    array_debug($obj);
 ?>
+
+<div class="post_title">
+    <?= $obj[0]->post_title ?>
+</div>
+
+<div class="post_content">
+    <?= $obj[0]->post_content ?>  
+</div>
+
+<div class="post_add_info">
+    <span>Pridal:</span>
+    <?= $obj[0]->post_author_id ?> ,
+    <?= $obj[0]->post_date ?> 
+</div>
+
+<div class="post_modifie_info">
+    <span>Posledná úprava:</span>
+    <?= $obj[0]->post_modifie_author_id ?> ,
+    <?= $obj[0]->post_modifie_date ?> 
+</div>
--- trunk/application/views/project_categories_detail_view.php
+++ trunk/application/views/project_categories_detail_view.php
@@ -1,7 +1,77 @@
 <?php
+    $obj = $this->selecter->get_category_detail(1);
+    array_debug($obj);
+?>
+Kategoria: 
+<div class="project_category_name">
+    <?= $obj[0]->project_category_name ?>  
+</div>
+Štatistiky:
+<div class="project_category_cash">
+    Súčasný stav:<?= $obj[0]->project_category_cash ?> 
+    Presun sem:<?= $obj[0]->transaction_cash_from ?>
+    presun preč:<?= $obj[0]->transaction_cash_to ?>
+</div>
+Transakcie
+<div class="errors">
+    <?php echo validation_errors();
+    $field = $this->selecter->id($event_id,'events','event_id');
+    //array_debug($programs) 
+    ?>
+</div>
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+<?= form_open("events/edit") ?>
+    Odkiaľ:
+    <div class="inputitem">
+        <label for="event_categories_id" class="<?= $error['event_categories_id'] ?>"><?= $this->lang->line('label_event_category_id') ?></label>
+        <?= gen_dropdown('event_categories_id', set_value('event_categories_id', $field->event_event_category_id),$this->selecter->get_event_categories(),'event_category_id','event_category_name'); ?>
+    </div>
+    Kam:
+    <div class="inputitem">
+        <label for="event_categories_id" class="<?= $error['event_categories_id'] ?>"><?= $this->lang->line('label_event_category_id') ?></label>
+        <?= gen_dropdown('event_categories_id', set_value('event_categories_id'),$this->selecter->get_event_categories(),'event_category_id','event_category_name'); ?>     
+    </div>
+    Koľko:
+    <div class="inputitem">
+        <label for="" class="<?= $error[''] ?>"><?= $this->lang->line('label_') ?></label>
+        <?= form_input(array('name' => '', 'id' => '', 'class' => ''.$error['']), set_value('')) ?>
+    </div>   
+    
+    <div class="inputitem">
+        <?= form_submit(array('type'=>'submit', 'name' => 'submit'), $this->lang->line('')) ?>
+    </div>
+<?= form_close() ?>
+
+ <?php
+    $this->load->library('grid');
+    
+    $grid = new Grid();
+    
+    array_debug($this->selecter->get_transactions(1));
+    
+    $grid->bind($this->selecter->get_transactions(1), 'pr_cat_id');
+    
+    $grid->add_url = "{$this->router->class}/add";
+    $grid->edit_url = "{$this->router->class}/edit";
+    $grid->remove_url = "{$this->router->class}/delete";
+    
+    $grid->header('pr_cat_id')->editable = false;
+    	
+    $grid->display();
+    
+    echo '<br />';
+    
+    $grid1 = new Grid();
+    
+    array_debug($this->selecter->get_projects(1));
+    
+    $grid1->bind($this->selecter->get_projects(1), 'cat_id');
+    
+    $grid1->add_url = "{$this->router->class}/add";
+    $grid1->edit_url = "{$this->router->class}/edit";
+    $grid1->remove_url = "{$this->router->class}/delete";
+    
+    $grid1->header('cat_id')->editable = false;
+    	
+    $grid1->display();
 ?>
