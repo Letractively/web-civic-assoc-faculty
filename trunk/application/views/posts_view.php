3a4c6ab4040b141382fad890c0ed709da33b38df
pitukemaster@gmail.com <pitukemaster@gmail.com@0f6ebd93-bc43-3e5c-799d-1a7754f0039d>
2012-11-29 01:31:44 +0000

    Matej: prerabane po Minovi posts_detail_view.php, posts_view.php, drobny fix selecter, config.php vrateny do povodneho stavu

--- trunk/application/views/posts_view.php
+++ trunk/application/views/posts_view.php
@@ -1,25 +1,43 @@
 <?php
     $obj = $this->selecter->get_posts();
-    array_debug($obj);
+    
+    //array_debug($obj);
+    
+    //$date = datetime($o->post_modifie_date, FALSE);
+    //$time = time_withou_seconds(datetime($o->post_modifie_date, TRUE));
+
+    foreach($obj as $o)
+    {
 ?>
+        <div class="post_title">
+            <?= anchor('posts/detail/'.$o->post_id,$o->post_title) ?>
+        </div>
 
-<div class="post_title">
-    <?= $obj[0]->post_title ?>
-</div>
+        <div class="post_content">
+            <?= $o->post_content ?>  
+        </div>
 
-<div class="post_content">
-    <?= $obj[0]->post_content ?>  
-</div>
+        <div class="post_add_info">
+            <span>Pridal:</span>
+            <?php 
+                echo $o->author_name.' '.$o->author_surname.', ';
+                echo $o->post_date;
+            ?> 
+        </div>
 
-<div class="post_add_info">
-    <span>Pridal:</span>
-    <?= $obj[0]->post_author_id ?> ,
-    <?= $obj[0]->post_date ?> 
-</div>
-
-<div class="post_modifie_info">
-    <span>Posledná úprava:</span>
-    <?= $obj[0]->post_modifie_author_id ?> ,
-    <?= $obj[0]->post_modifie_date ?> 
-</div>
-<hr>
+        <div class="post_modifie_info">
+            <?php 
+                if($o->post_modifie_author_id == TRUE || $o->post_modifie_date == TRUE)
+                {
+                    echo '<span>Posledná úprava:</span>';
+                    echo $o->modifier_name.' '.$o->modifier_surname.', ';
+                }
+                if($o->post_modifie_date)
+                {
+                    echo datetime($o->post_modifie_date, FALSE).' '.time_withou_seconds(datetime($o->post_modifie_date, TRUE));
+                } 
+            ?> 
+        </div>
+<?
+    }
+?>
