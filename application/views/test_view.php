e0dcc93d92a58ce9bc8e554e6238dcaf6cdbe841
pitukemaster <pitukemaster@gmail.com>
2012-12-03 22:29:37 +0000

    Stevo: zmazanie velkych obrazkov, rozbehany prazdny bind na gride

--- application/views/test_view.php
+++ application/views/test_view.php
@@ -6,7 +6,9 @@
 	$grid = new Grid();
 	
 	//$grid->bind($this->db->query("SELECT u.user_id, u.user_name, u.user_surname, u.user_email, sp.study_program_name, d.degree_name, u.user_postcode FROM users u, degrees d, study_programs sp WHERE u.user_degree_id = d.degree_id && u.user_study_program_id = sp.study_program_id ORDER BY user_id")->result(), 'user_id');
-	$grid->bind($this->selecter->get_degrees(), 'degree_id');
+	//$grid->bind($this->selecter->get_degrees(), 'degree_id');
+	$empty = array();
+	//$grid->bind($empty, 'degree_id');
 	
 	$grid->add_url = "{$this->router->class}/add";
 	$grid->edit_url = "{$this->router->class}/edit";
