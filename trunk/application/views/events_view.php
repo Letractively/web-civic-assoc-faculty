6531e29b4f4e4712e93b1fcb9e3a0bc1e3b7c585
pitukemaster@gmail.com <pitukemaster@gmail.com@0f6ebd93-bc43-3e5c-799d-1a7754f0039d>
2012-12-08 23:23:37 +0000

    Matej: fix

--- trunk/application/views/events_view.php
+++ trunk/application/views/events_view.php
@@ -3,19 +3,19 @@
     
     $grid = new Grid();
     
-    array_debug($this->selecter->get_events(0));
-    array_debug($priorities);
+    //array_debug($this->selecter->get_events(0));
+    //array_debug($priorities);
 
     if( $grid->bind($this->selecter->get_events($event_id), 'event_id') )
     {
-        $grid->add_url = "{$this->router->class}/add";
-        $grid->edit_url = "{$this->router->class}/edit";
-        $grid->remove_url = "{$this->router->class}/delete";
-        $grid->header('event_category_name')->component->type = "combobox";
-        $grid->header('event_category_name')->component->bind( $this->selecter->get_event_categories(), "event_category_id", "event_category_name");
-        $grid->header('event_priority')->component->type = "combobox";
-        $grid->header('event_priority')->component->bind($priorities, 'id', 'value');
         $grid->header('event_id')->editable = false;
+        
+        $grid->header('event_id')->visible = false;
+        $grid->header('event_about')->visible = false;
+        $grid->header('event_created')->visible = false;
+        $grid->header('event_category_name')->visible = false;
+        $grid->header('event_priority')->visible = false;
+        
         $grid->display();
     }
      
