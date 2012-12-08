03640f23d8239b8e89b678bb756dbb21a90ebbd6
pitukemaster@gmail.com <pitukemaster@gmail.com@0f6ebd93-bc43-3e5c-799d-1a7754f0039d>
2012-12-08 18:47:00 +0000

    Mino

--- trunk/application/views/correspondence_review_view.php
+++ trunk/application/views/correspondence_review_view.php
@@ -2,16 +2,15 @@
     $this->load->library('grid');
     
     $grid = new Grid();
+
     
-    //array_debug($this->selecter->get_events(1));
-    
-    $grid->bind($this->selecter->get_user($study_ids, $degrees, $degree_years),'study_ids', 'degrees', 'degree_years');
+    $grid->bind($this->selecter->get_users($study_ids, $degrees, $degree_years),'study_ids', 'degrees', 'degree_years');
     
     $grid->add_url = "{$this->router->class}/add";
     $grid->edit_url = "{$this->router->class}/edit";
     $grid->remove_url = "{$this->router->class}/delete";
     
-    $grid->header('event_id')->editable = false;
+    $grid->header('user_id')->editable = false;
     
 	
     $grid->display();
