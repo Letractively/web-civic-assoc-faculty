65f7c3a26ef9ff9d417d4bf740ea4afc73f5f82e
pitukemaster <pitukemaster@gmail.com>
2012-12-08 19:32:21 +0000

    Matej: bbcode update, som zabudol pridat do commitu promin stevo, Uz je to ok a zapni si v autoloade bbcode inak fixnute events

---
+++ application/helpers/bbcode_helper.php
@@ -0,0 +1,188 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 4.3.2 or newer
+ *
+ * @package        CodeIgniter
+ * @author        Rick Ellis
+ * @copyright    Copyright (c) 2006, EllisLab, Inc.
+ * @license        http://www.codeignitor.com/user_guide/license.html
+ * @link        http://www.codeigniter.com
+ * @since        Version 1.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * CodeIgniter bbCode Helpers
+ *
+ * @package        CodeIgniter
+ * @subpackage    Helpers
+ * @category    Helpers
+ * @author        Santoni Jean-André
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * JS Insert bbCode
+ *
+ * Generates the javascrip function needed to insert bbcodes into a form field
+ *
+ * @access    public
+ * @param    string    form name
+ * @param    string    field name
+ * @return    string
+ */    
+
+function js_insert_bbcode($form_name = '', $form_field = '')
+{
+    ?>
+    <script type="text/javascript">
+    function insert_bbcode(open, close)
+    {
+		var obsah = document.getElementById("textarea");
+		obsah.value =  obsah.value + open + close;
+	}
+	
+    </script> 
+    <?php 
+} 
+ 
+
+
+// ------------------------------------------------------------------------
+
+/**
+ * Parse bbCode
+ *
+ * Takes a string as input and replace bbCode by (x)HTML tags
+ *
+ * @access    public
+ * @param    string    the text to be parsed
+ * @return    string
+ */    
+function parse_bbcode($str, $clear = 0, $bbcode_to_parse = NULL)
+{
+    if ( ! is_array($bbcode_to_parse))
+    {
+        if (FALSE === ($bbcode_to_parse = _get_bbcode_to_parse_array()))
+        {
+            return FALSE;
+        }        
+    }
+    
+    foreach ($bbcode_to_parse as $key => $val)
+    {        
+        for ($i = 1; $i <= $bbcode_to_parse[$key][2]; $i++) // loop for imbricated tags
+        {
+            $str = preg_replace($key, $bbcode_to_parse[$key][$clear], $str);
+        }
+    }
+
+    return $str;
+}
+
+// ------------------------------------------------------------------------
+
+/**
+ * Clear bbCode
+ *
+ * Takes a string as input and remove bbCode tags
+ *
+ * @access    public
+ * @param    string    the text to be parsed
+ * @return    string
+ */    
+function clear_bbcode($str)
+{
+    return parse_bbcode($str, 1);
+}
+
+// ------------------------------------------------------------------------
+
+/**
+ * Get bbCode Buttons
+ *
+ * Returns an array of bbcode buttons that can be clicked to be inserted 
+ * into a form field.  
+ *
+ * @access    public
+ * @return    array
+ */    
+
+function get_bbcode_buttons($bbcode = NULL)
+{
+    if ( ! is_array($bbcode))
+    {
+        if (FALSE === ($bbcode = _get_bbcode_array()))
+        {
+            return $str;
+        }        
+    }
+
+    foreach ($bbcode as $key => $val)
+    {
+        $button[] = '<input type="button" class="button" id="'.$key.'" name="'.$key.'" value="'.$key.'" onClick="'.$val.'" />';
+    }
+    
+    return $button;
+}
+
+// ------------------------------------------------------------------------
+
+/**
+ * Get bbCode Array
+ *
+ * Fetches the config/bbcode.php file
+ *
+ * @access    private
+ * @return    mixed
+ */    
+function _get_bbcode_array()
+{
+    if ( ! file_exists(APPPATH.'config/bbcode'.EXT))
+    {
+        return FALSE;
+    }
+
+    include(APPPATH.'config/bbcode'.EXT);
+
+    if ( ! isset($bbcode) OR ! is_array($bbcode))
+    {
+        return FALSE;
+    }
+    
+    return $bbcode;
+}
+
+// ------------------------------------------------------------------------
+
+/**
+ * Get bbCode Array for parsing
+ *
+ * Fetches the config/bbcode.php file
+ *
+ * @access    private
+ * @return    mixed
+ */    
+function _get_bbcode_to_parse_array()
+{
+    if ( ! file_exists(APPPATH.'config/bbcode'.EXT))
+    {
+        return FALSE;
+    }
+
+    include(APPPATH.'config/bbcode'.EXT);
+    
+    if ( ! isset($bbcode_to_parse) OR ! is_array($bbcode_to_parse))
+    {
+        return FALSE;
+    }
+    
+    return $bbcode_to_parse;
+}
+
+?> 
