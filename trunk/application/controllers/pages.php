57d386024d7ca0cd49122d1fc18abe75e0f78194
pitukemaster@gmail.com <pitukemaster@gmail.com@0f6ebd93-bc43-3e5c-799d-1a7754f0039d>
2012-12-09 13:59:05 +0000

    Stevo: pridane pages pre mata

---
+++ trunk/application/controllers/pages.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Pages extends MY_Controller
+{
+    
+        /*
+         * __construct
+         * 
+         * Konštruktor triedy
+         * 
+         */
+        function __construct() 
+        {
+            parent::__construct();
+        }
+
+        /*
+         * index
+         * 
+         * default index metoda, ktora sa vola primarne
+         * 
+         */
+        public function index($view)
+        {
+			$this->load->view($this->router->class.'_'.$view.'_'.'view');
+        }
+}
