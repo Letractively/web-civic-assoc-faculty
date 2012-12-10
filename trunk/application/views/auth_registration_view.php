3a980de674be9482b44cc53501a9a8239c3a113e
pitukemaster@gmail.com <pitukemaster@gmail.com@0f6ebd93-bc43-3e5c-799d-1a7754f0039d>
2012-12-10 06:25:18 +0000

    Mato - zopár formulárov na demo prezentáciu 	- header_view - spravený login/logout panel 	- registration_view 	- correspondence_view 	- events_edit_view

--- trunk/application/views/auth_registration_view.php
+++ trunk/application/views/auth_registration_view.php
@@ -2,88 +2,89 @@
     <?php 
         echo validation_errors();     
      ?>
-
 </div>
 
-<?= form_open("auth/registration") ?>
-    <div class="inputitem">
-        <p class="label"> <label for="name" class="<?= $error['name'] ?>"><?= $this->lang->line('label_name') ?></label> </p>
-        <p class="box"> <?= form_input(array('name' => 'name', 'id' => 'name', 'class' => ''.$error['name']), set_value('name')) ?> </p>
-    </div>
-    <div class="inputitem">
-        <p class="label"> <label for="surname" class="<?= $error['surname'] ?>"><?= $this->lang->line('label_surname') ?></label> </p>
-        <p class="box"> <?= form_input(array('name' => 'surname', 'id' => 'surname', 'class' => ''.$error['surname']), set_value('surname')) ?> </p>
-    </div>
-    <div class="inputitem">
-        <p class="label"> <label for="username" class="<?= $error['username'] ?>"><?= $this->lang->line('label_username') ?></label> </p>
-        <p class="box"> <?= form_input(array('name' => 'username', 'id' => 'username', 'class' => ''.$error['username']), set_value('username')) ?> </p>
-    </div>
-    <div class="inputitem">
-        <p class="label"> <label for="password" class="<?= $error['password'] ?>"><?= $this->lang->line('label_password') ?></label> </p>
-        <p class="box"> <?= form_password(array('name' => 'password', 'id' => 'password', 'class' => ''.$error['password']), set_value('password')) ?> </p>
-    </div>
-    <div class="inputitem">
-        <p class="label"> <label for="password_again" class="<?= $error['password_again'] ?>"><?= $this->lang->line('label_password_again') ?></label> </p>
-        <p class="box"> <?= form_password(array('name' => 'password_again', 'id' => 'password_again', 'class' => ''.$error['password_again']), set_value('password_again')) ?> </p>
-    </div>
-    <div class="inputitem">
-        <p class="label"> <label for="email" class="<?= $error['email'] ?>"><?= $this->lang->line('label_email') ?></label> </p>
-        <p class="box"> <?= form_input(array('name' => 'email', 'id' => 'email', 'class' => ''.$error['email']), set_value('email')) ?> </p>
-    </div>
-    <div class="inputitem">
-        <p class="label"> <label for="phone" class="<?= $error['phone'] ?>"><?= $this->lang->line('label_phone') ?></label> </p>
-        <p class="box"> <?= form_input(array('name' => 'phone', 'id' => 'phone', 'class' => ''.$error['phone']), set_value('phone')) ?> </p>
-    </div>
-    <div class="inputitem">
-        <p class="label"> <label for="study_program_id" class="<?= $error['study_program_id'] ?>"><?= $this->lang->line('label_study_program_id') ?></label> </p>
-        <p class="box"> <?= gen_dropdown('study_program_id', set_value('study_program_id'), $this->selecter->get_study_programs(), 'study_program_id', 'study_program_name'); ?> </p>
-    </div>
-    <div class="inputitem">
-        <p class="label"> <label for="degree_id" class="<?= $error['degree_id'] ?>"><?= $this->lang->line('label_degree_id') ?></label> </p>
-        <p class="box"> <?= gen_dropdown('degree_id', set_value('degree_id'), $this->selecter->get_degrees(), 'degree_id', 'degree_name'); ?> </p>
-    </div>
-    <div class="inputitem">
-        <p class="label"> <label for="place_of_birth" class="<?= $error['place_of_birth'] ?>"><?= $this->lang->line('label_place_of_birth') ?></label> </p>
-        <p class="box"> <?= form_input(array('name' => 'place_of_birth', 'id' => 'place_of_birth', 'class' => ''.$error['place_of_birth']), set_value('plac-e_of_birth')) ?> </p>
-    </div>
-    <div class="inputitem">
-        <p class="label"> <label for="postcode" class="<?= $error['postcode'] ?>"><?= $this->lang->line('label_postcode') ?></label> </p>
-        <p class="box"> <?= form_input(array('name' => 'postcode', 'id' => 'postcode', 'class' => ''.$error['postcode']), set_value('postcode')) ?> </p>
-    </div>
-    <div class="inputitem">
-        <p class="label"> <label for="degree_year" class="<?= $error['degree_year'] ?>"><?= $this->lang->line('label_degree_year') ?></label> </p>
-        <p class="box"> <?= form_dropdown('degree_year', $years, set_value('degree_year_id')) ?> </p>
-    </div>
- 
-    <div class="inputitem">
-        <p class="label"> <label for="total_sum" class=" <?= $error['total_sum'] ?>"><?= $this->lang->line('label_total_tum') ?></label> </p>
-        <p class="box"> <?= form_input(array('name' => 'total_sum', 'id' => 'total_sum', 'class'=>''.$error['total_sum']), set_value('total_sum')) ?> </p>
-    </div>
-    <div class="inputitem">
-        <p class="label"> <label for="vs" class=" <?= $error['vs'] ?>"><?= $this->lang->line('label_vs') ?></label> </p>
-        <p class="box"> <?= form_input(array('name' => 'vs', 'id' => 'vs', 'class'=>''.$error['vs']), set_value('vs')) ?> </p>
-    </div>
+<div id="form_wrapper">
+	<?= form_open("auth/registration") ?>
+		<div class="inputitem">
+			<p class="label"> <label for="name" class="<?= $error['name'] ?>"><?= $this->lang->line('label_name') ?></label> </p>
+			<?= form_input(array('name' => 'name', 'id' => 'name', 'class' => 'input_data'.$error['name']), set_value('name')) ?>
+		</div>
+		<div class="inputitem">
+			<p class="label"> <label for="surname" class="<?= $error['surname'] ?>"><?= $this->lang->line('label_surname') ?></label> </p>
+			<?= form_input(array('name' => 'surname', 'id' => 'surname', 'class' => 'input_data'.$error['surname']), set_value('surname')) ?>
+		</div>
+		<div class="inputitem">
+			<p class="label"> <label for="username" class="<?= $error['username'] ?>"><?= $this->lang->line('label_username') ?></label> </p>
+			<?= form_input(array('name' => 'username', 'id' => 'username', 'class' => 'input_data'.$error['username']), set_value('username')) ?>
+		</div>
+		<div class="inputitem">
+			<p class="label"> <label for="password" class="<?= $error['password'] ?>"><?= $this->lang->line('label_password') ?></label> </p>
+			<?= form_password(array('name' => 'password', 'id' => 'password', 'class' => 'input_data'.$error['password']), set_value('password')) ?>
+		</div>
+		<div class="inputitem">
+			<p class="label"> <label for="password_again" class="<?= $error['password_again'] ?>"><?= $this->lang->line('label_password_again') ?></label> </p>
+			<?= form_password(array('name' => 'password_again', 'id' => 'password_again', 'class' => 'input_data'.$error['password_again']), set_value('password_again')) ?>
+		</div>
+		<div class="inputitem">
+			<p class="label"> <label for="email" class="<?= $error['email'] ?>"><?= $this->lang->line('label_email') ?></label> </p>
+			<?= form_input(array('name' => 'email', 'id' => 'email', 'class' => 'input_data'.$error['email']), set_value('email')) ?> 
+		</div>
+		<div class="inputitem">
+			<p class="label"> <label for="phone" class="<?= $error['phone'] ?>"><?= $this->lang->line('label_phone') ?></label> </p>
+			<?= form_input(array('name' => 'phone', 'id' => 'phone', 'class' => 'input_data'.$error['phone']), set_value('phone')) ?>
+		</div>
+		<div class="inputitem">
+			<p class="label"> <label for="study_program_id" class="<?= $error['study_program_id'] ?>"><?= $this->lang->line('label_study_program_id') ?></label> </p>
+			<?= gen_dropdown('study_program_id', set_value('study_program_id'), $this->selecter->get_study_programs(), 'study_program_id', 'study_program_name'); ?>
+		</div>
+		<div class="inputitem">
+			<p class="label"> <label for="degree_id" class="<?= $error['degree_id'] ?>"><?= $this->lang->line('label_degree_id') ?></label> </p>
+			<?= gen_dropdown('degree_id', set_value('degree_id'), $this->selecter->get_degrees(), 'degree_id', 'degree_name'); ?>
+		</div>
+		<div class="inputitem">
+			<p class="label"> <label for="place_of_birth" class="<?= $error['place_of_birth'] ?>"><?= $this->lang->line('label_place_of_birth') ?></label> </p>
+			<?= form_input(array('name' => 'place_of_birth', 'id' => 'place_of_birth', 'class' => 'input_data'.$error['place_of_birth']), set_value('plac-e_of_birth')) ?>
+		</div>
+		<div class="inputitem">
+			<p class="label"> <label for="postcode" class="<?= $error['postcode'] ?>"><?= $this->lang->line('label_postcode') ?></label> </p>
+			<?= form_input(array('name' => 'postcode', 'id' => 'postcode', 'class' => 'input_data'.$error['postcode']), set_value('postcode')) ?>
+		</div>
+		<div class="inputitem">
+			<p class="label"> <label for="degree_year" class="<?= $error['degree_year'] ?>"><?= $this->lang->line('label_degree_year') ?></label> </p>
+			<?= form_dropdown('degree_year', $years, set_value('degree_year_id')) ?>
+		</div>
+	 
+		<div class="inputitem">
+			<p class="label"> <label for="total_sum" class=" <?= $error['total_sum'] ?>"><?= $this->lang->line('label_total_tum') ?></label> </p>
+			<?= form_input(array('name' => 'total_sum', 'id' => 'total_sum', 'class'=>'input_data'.$error['total_sum']), set_value('total_sum')) ?>
+		</div>
+		<div class="inputitem">
+			<p class="label"> <label for="vs" class=" <?= $error['vs'] ?>"><?= $this->lang->line('label_vs') ?></label> </p>
+			<?= form_input(array('name' => 'vs', 'id' => 'vs', 'class'=>'input_data'.$error['vs']), set_value('vs')) ?>
+		</div>
 
-    <?php
-       $obj = $this->selecter->get_project_categories();
-       
-       $i = 1;
-       echo '<table>';
-            echo '<tr><th>'.$this->lang->line('table_th_category').'</th><th>'.$this->lang->line('table_th_ratio').'</th></tr>';
-            foreach($obj as $o)
-            {
-                echo '<tr>';
-                     echo '<td><label for="project_category_'.$i.'">';
-                         echo $o->project_category_name;
-                     echo '</label></td>';
-                     echo '<td>'.form_input(array('name' => 'project_category_'.$i, 'id' => 'project_category_'.$i, 'size'=> 1 ), set_value('project_category_'.$i)).'</td>';
-                echo '</tr>';
-                $i++;
-            }
-       echo '</table>';
-    ?>
+		<?php
+		   $obj = $this->selecter->get_project_categories();
+		   
+		   $i = 1;
+		   echo '<table class="inputitem">';
+				echo '<tr><th>'.$this->lang->line('table_th_category').'</th><th>'.$this->lang->line('table_th_ratio').'</th></tr>';
+				foreach($obj as $o)
+				{
+					echo '<tr>';
+						 echo '<td> <label for="project_category_'.$i.'">';
+							 echo $o->project_category_name;
+						 echo '</label></td>';
+						 echo '<td>'.form_input(array('name' => 'project_category_'.$i, 'id' => 'project_category_'.$i, 'size'=> 3, 'class' => 'input_data_reg' ), set_value('project_category_'.$i)).'</td>';
+					echo '</tr>';
+					$i++;
+				}
+		   echo '</table>';
+		?>
 
-    <div class="inputitem">
-        <?= form_submit(array('type'=>'submit', 'name' => 'submit'), $this->lang->line('button_register')) ?>
-    </div>
-<?= form_close() ?>
+		<div class="inputitem">
+			<?= form_submit(array('type'=>'submit', 'name' => 'submit', 'class' => 'button_submit'), $this->lang->line('button_register')) ?>
+		</div>
+	<?= form_close() ?>
+</div>
--- trunk/application/views/events_edit_view.php
+++ trunk/application/views/events_edit_view.php
@@ -5,53 +5,55 @@
     ?>
 </div>
 <?= js_insert_bbcode('events/edit', 'textarea'); ?>
-<?= form_open("events/edit/".$event_id) ?>
-    <div class="inputitem">
-        <label for="event_category_id" class="<?= $error['event_category_id'] ?>"><?= $this->lang->line('label_event_category_id') ?></label>
-        <?= gen_dropdown('event_category_id', set_value('event_category_id', $field->event_event_category_id),$this->selecter->get_event_categories(),'event_category_id','event_category_name'); ?>
-    </div>
+<div id="form_wrapper">
+	<?= form_open("events/edit/".$event_id) ?>
+		<div class="inputitem">
+			<p class="label"> <label for="event_category_id" class="<?= $error['event_category_id'] ?>"><?= $this->lang->line('label_event_category_id') ?></label> </p>
+			<?= gen_dropdown('event_category_id', set_value('event_category_id', $field->event_event_category_id),$this->selecter->get_event_categories(),'event_category_id','event_category_name'); ?>
+		</div>
 
- <!--generovat prioritu-->
-    <div class="inputitem">
-        <label for="priority" class="<?= $error['priority'] ?>"><?= $this->lang->line('label_priority') ?></label>
-        <?= gen_dropdown('priority', set_value('priority_id'), $priorities, 'id', 'value'); ?>
-    </div>
- <!--end-->
- 
-    <div class="inputitem">
-        <label for="name" class="<?= $error['name'] ?>"><?= $this->lang->line('label_name') ?></label>
-        <?= form_input(array('name' => 'name', 'id' => 'name', 'class' => ''.$error['name']), set_value('name', $field->event_name)) ?>
-    </div>
- 
-<!--od -> do-->
-    <div class="inputitem">
-        <label for="from" class="<?= $error['from'] ?>"><?= $this->lang->line('label_from') ?></label>
-        <?= form_input(array('name' => 'from', 'id' => 'from', 'class' => ''.$error['from'], 'maxlength' => 10, 'size' => 10), set_value('from', datetime($field->event_from, FALSE))) ?>
-        <?= form_input(array('name' => 'from_time', 'maxlength' => 5, 'size' => 5), set_value('from_time',  time_withou_seconds(datetime($field->event_from, TRUE)))); ?>
-    </div>
- 
-    <div class="inputitem">
-        <label for="to" class="<?= $error['to'] ?>"><?= $this->lang->line('label_to') ?></label>
-        <?= form_input(array('name' => 'to', 'id' => 'to', 'class' => ''.$error['to'], 'maxlength' => 10, 'size' => 10), set_value('to',  datetime($field->event_to, FALSE))) ?>
-        <?= form_input(array('name' => 'to_time', 'maxlength' => 5, 'size' => 5), set_value('to_time', time_withou_seconds(datetime($field->event_to, TRUE)))); ?>
-    </div>
-<!--end-->
-    <div>
-        <?php
-            foreach($buttons as $i)
-            {
-                echo $i;
-            }
-        ?>
-    </div>  
-    <div class="inputitem">
-        <label for="about" class="<?= $error['about'] ?>"><?= $this->lang->line('label_about') ?></label>
-        <?= form_textarea(array('name' => 'about', 'id' => 'textarea', 'class' => ''.$error['about']), set_value('about', $field->event_about)) ?>
-    </div>
+	 <!--generovat prioritu-->
+		<div class="inputitem">
+			<p class="label"> <label for="priority" class="<?= $error['priority'] ?>"><?= $this->lang->line('label_priority') ?></label> </p>
+			<?= gen_dropdown('priority', set_value('priority_id'), $priorities, 'id', 'value'); ?>
+		</div>
+	 <!--end-->
+	 
+		<div class="inputitem">
+			<p class="label"> <label for="name" class="<?= $error['name'] ?>"><?= $this->lang->line('label_name') ?></label> </p>
+			<?= form_input(array('name' => 'name', 'id' => 'name', 'class' => 'input_data'.$error['name']), set_value('name', $field->event_name)) ?>
+		</div>
+	 
+	<!--od -> do-->
+		<div class="inputitem">
+			<p class="label"> <label for="from" class="<?= $error['from'] ?>"><?= $this->lang->line('label_from') ?></label> </p>
+			<?= form_input(array('name' => 'from', 'id' => 'from', 'class' => 'input_data_date'.$error['from'], 'maxlength' => 10, 'size' => 10), set_value('from', datetime($field->event_from, FALSE))) ?>
+			<?= form_input(array('name' => 'from_time', 'maxlength' => 5, 'size' => 5, 'class' => 'input_data_time'), set_value('from_time',  time_withou_seconds(datetime($field->event_from, TRUE)))); ?>
+		</div>
+	 
+		<div class="inputitem">
+			<p class="label"> <label for="to" class="<?= $error['to'] ?>"><?= $this->lang->line('label_to') ?></label> </p>
+			<?= form_input(array('name' => 'to', 'id' => 'to', 'class' => 'input_data_date'.$error['to'], 'maxlength' => 10, 'size' => 10), set_value('to',  datetime($field->event_to, FALSE))) ?>
+			<?= form_input(array('name' => 'to_time', 'maxlength' => 5, 'size' => 5, 'class' => 'input_data_time'), set_value('to_time', time_withou_seconds(datetime($field->event_to, TRUE)))); ?>
+		</div>
+	<!--end-->
+		<div>
+			<?php
+				foreach($buttons as $i)
+				{
+					echo $i;
+				}
+			?>
+		</div>  
+		<div class="inputitem">
+			<p class="label"> <label for="about" class="<?= $error['about'] ?>"><?= $this->lang->line('label_about') ?></label> </p>
+			<?= form_textarea(array('name' => 'about', 'id' => 'textarea', 'class' => 'textarea_data'.$error['about']), set_value('about', $field->event_about)) ?>
+		</div>
 
-    
+		
 
-    <div class="inputitem">
-        <?= form_submit(array('type'=>'submit', 'name' => 'submit'), $this->lang->line('button_edit_event')) ?>
-    </div>
-<?= form_close() ?>
+		<div class="inputitem">
+			<?= form_submit(array('type'=>'submit', 'name' => 'submit', 'class' => 'button_submit'), $this->lang->line('button_edit_event')) ?>
+		</div>
+	<?= form_close() ?>
+</div>
