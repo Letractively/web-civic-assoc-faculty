ba9a92a99a0ad8573324b09caf95419ee97db446
pitukemaster@gmail.com <pitukemaster@gmail.com@0f6ebd93-bc43-3e5c-799d-1a7754f0039d>
2012-12-14 17:55:58 +0000

    Mino: post modifyers

--- trunk/application/views/posts_modifiers_view.php
+++ trunk/application/views/posts_modifiers_view.php
@@ -4,9 +4,7 @@
     $grid = new Grid();
     
     $grid->bind($this->selecter->get_post_modifiers($post_id), 'post_modifie_id');
-    $grid->header('post_modifie_id')->editable = false;
-    $grid->add_url = "{$this->router->class}/add";
-    $grid->edit_url = "{$this->router->class}/edit";
-    $grid->remove_url = "{$this->router->class}/delete";
+    $grid->header('user_id')->visible = false;
+    $grid->header('post_modifie_id')->visible = false;
     $grid->display();
 ?>
