9d784e9c3b03172e0c1c22021a35d7918ae78cb4
pitukemaster <pitukemaster@gmail.com>
2012-12-15 22:51:39 +0000

    Mino: posts_modifiers_view.php

--- application/views/posts_modifiers_view.php
+++ application/views/posts_modifiers_view.php
@@ -6,5 +6,9 @@
     $grid->bind($this->selecter->get_post_modifiers($post_id), 'post_modifie_id');
     $grid->header('user_id')->visible = false;
     $grid->header('post_modifie_id')->visible = false;
+    
+    $grid->header('user_name')->text = $this->lang->line('user_name');
+    $grid->header('user_surname')->text = $this->lang->line('user_surname');
+    $grid->header('post_modifie_date')->text = $this->lang->line('label_post_date');
     $grid->display();
 ?>
