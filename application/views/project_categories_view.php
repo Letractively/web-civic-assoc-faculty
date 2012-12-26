55080552e06b261764aef942d9bc5b47ffe9e8ab
pitukemaster <pitukemaster@gmail.com>
2012-12-26 22:39:28 +0000

    Maťo - pár malých úprav na CSS + project_categories_view

--- application/views/project_categories_view.php
+++ application/views/project_categories_view.php
@@ -1,27 +1,28 @@
-﻿ <?= $this->lang->line('label_together').': '.$this->selecter->get_project_categories_total_cash(); ?>
+﻿<div id="grid_wrapper">
+	<p class="project_label"> <?= $this->lang->line('label_together').': '.$this->selecter->get_project_categories_total_cash(); ?> </p>
 
-<?php
-    $this->load->library('grid');
+	<?php
+		$this->load->library('grid');
 
-    $grid = new Grid();
-    
-    $grid->bind($this->selecter->get_project_categories(), 'project_category_id');
-    
-    $grid->add_url = "{$this->router->class}/add";
-    $grid->edit_url = "{$this->router->class}/edit";
-    $grid->remove_url = "{$this->router->class}/delete";
-    
-    $grid->header('project_category_id')->editable = false;
-    $grid->header('project_category_cash')->editable = false;
-    
-    $grid->header('project_category_id')->visible = false;
-    
-    $grid->header('project_category_name')->text = $this->lang->line('label_name');
-    $grid->header('project_category_cash')->text = $this->lang->line('label_capital');
-    
-    $grid->header('project_category_name')->set_anchor('project_categories/detail', 'project_category_id');
-	
-    $grid->display();
-	
-	echo anchor(base_url()."projects", $this->lang->line('anchor_back'));
-?>
+		$grid = new Grid();
+		
+		$grid->bind($this->selecter->get_project_categories(), 'project_category_id');
+		
+		$grid->add_url = "{$this->router->class}/add";
+		$grid->edit_url = "{$this->router->class}/edit";
+		$grid->remove_url = "{$this->router->class}/delete";
+		
+		$grid->header('project_category_id')->editable = false;
+		$grid->header('project_category_cash')->editable = false;
+		
+		$grid->header('project_category_id')->visible = false;
+		
+		$grid->header('project_category_name')->text = $this->lang->line('label_name');
+		$grid->header('project_category_cash')->text = $this->lang->line('label_capital');
+		
+		$grid->header('project_category_name')->set_anchor('project_categories/detail', 'project_category_id');
+
+		$grid->display();
+		echo '<p class="button_back">'; echo anchor(base_url()."projects", $this->lang->line('anchor_back')); echo '</p>';
+	?>
+</div>
