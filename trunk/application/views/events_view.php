603163c4301882276725a4ae1c2875c71c6ca833
pitukemaster@gmail.com <pitukemaster@gmail.com@0f6ebd93-bc43-3e5c-799d-1a7754f0039d>
2013-01-26 01:19:20 +0000

    Stevo: events_view - iny pohlad pre admina, iny pre usera, fix SDD

--- trunk/application/views/events_view.php
+++ trunk/application/views/events_view.php
@@ -14,8 +14,11 @@
 <div id="grid_wrapper">
 	<?php
 		if( $this->userdata->is_admin() )
-		{
-			echo '<p class="button_edit">'; echo anchor('event_categories/', $this->lang->line('to_event_categories')); echo '</p>';
+		{ ?>
+			<p class="project_label">
+			<span class="link_text"> <?= anchor('event_categories', $this->lang->line('to_event_categories')); ?> </span>
+			</p>
+		<?php
 		}
 	?>
 	
@@ -35,57 +38,80 @@
 		<?= gen_dropdown('event_category', $event_cat_id, $event_cats, 'event_category_id', 'event_category_name', 'dropdown', 'id="event_category" onchange="changeFilter(this);"'); ?>
 		
 	<?php
-		$this->load->library('grid');
-		
-		$grid = new Grid();
-		$method = '';
+		$event_list = null;
 		
 		switch ($flag)
 		{
 			case 0:
-				if ( $grid->bind($this->selecter->get_events($event_cat_id), 'event_id') )
-					$method = 'get_events';
+				$event_list = $this->selecter->get_events($event_cat_id);
 				break;
 			case 1:
-				if ( $grid->bind($this->selecter->get_events_newest($event_cat_id), 'event_id') )
-				$method = 'get_events_newest';
+				$event_list = $this->selecter->get_events_newest($event_cat_id);
 				break;
 			case 2:
-				if ( $grid->bind($this->selecter->get_events_prior($event_cat_id), 'event_id') )
-				$method = 'get_events_prior';
+				$event_list = $this->selecter->get_events_prior($event_cat_id);
 				break;
 		}
-		
-		if( $method != '' )
+	
+		if ($this->userdata->is_admin())
 		{
-			$grid->header('event_id')->editable = false;
-			$grid->header('event_id')->visible              = false;
-			$grid->header('event_about')->visible           = false;
-			$grid->header('event_created')->visible         = false;
-                        $grid->header('event_category_id')->visible     = false;
-			$grid->header('event_category_name')->text = $this->lang->line('label_catname');
-			$grid->header('event_priority')->visible        = false;
-			$grid->header('event_name')->set_anchor('events/detail', 'event_id');
-			$grid->header('event_name')->text   = $this->lang->line('label_name');
-			$grid->header('event_from')->set_datetime();
-			$grid->header('event_from')->text   = $this->lang->line('label_from');
-			$grid->header('event_to')->set_datetime();
-			$grid->header('event_to')->text     = $this->lang->line('label_to');
-                        
-                        //$grid->add_url = "{$this->router->class}/add";
-                        $grid->edit_url = "{$this->router->class}/edit";
-                        $grid->edit_mode = "external";
-                        $grid->remove_url = "{$this->router->class}/delete";
-		}
+			$this->load->library('grid');
+			$grid = new Grid();
+			
+			if( $grid->bind($event_list, 'event_id') )
+			{
+				$grid->header('event_id')->editable = false;
+				$grid->header('event_id')->visible              = false;
+				$grid->header('event_about')->visible           = false;
+				$grid->header('event_created')->visible         = false;
+							$grid->header('event_category_id')->visible     = false;
+				$grid->header('event_category_name')->text = $this->lang->line('label_catname');
+				$grid->header('event_priority')->visible        = false;
+				$grid->header('event_name')->set_anchor('events/detail', 'event_id');
+				$grid->header('event_name')->text   = $this->lang->line('label_name');
+				$grid->header('event_from')->set_datetime();
+				$grid->header('event_from')->text   = $this->lang->line('label_from');
+				$grid->header('event_to')->set_datetime();
+				$grid->header('event_to')->text     = $this->lang->line('label_to');
+							
+							//$grid->add_url = "{$this->router->class}/add";
+							$grid->edit_url = "{$this->router->class}/edit";
+							$grid->edit_mode = "external";
+							$grid->remove_url = "{$this->router->class}/delete";
+			}
 
-		$grid->display();		
-		/*if($event_cat_id != 0)
-			{ echo '<p class="button_edit">'; echo anchor('events/', $this->lang->line('back_to_event_categories')); echo '</p>'; }*/
-		if( $this->userdata->is_admin() )
-                {
-                    echo '<p class="button_edit">'; echo anchor('events/add', $this->lang->line('anchor_add')); echo '</p>';
-                }
-		
+			$grid->display();		
+			/*if($event_cat_id != 0)
+				{ echo '<p class="button_edit">'; echo anchor('events/', $this->lang->line('back_to_event_categories')); echo '</p>'; }*/
+			if( $this->userdata->is_admin() )
+				echo '<p class="button_edit">'; echo anchor('events/add', $this->lang->line('anchor_add')); echo '</p>';
+		}
+		else
+		{
+			foreach ($event_list as $event)
+			{ ?>
+			<p>
+				<div class="post_title">
+					<?= anchor('events/detail/'.$event->event_id, $event->event_name) ?>
+				</div>
+				
+				<div class="post_content">
+					<?= perex_from_content($event->event_about) ?>...  
+				</div>
+				
+				<div class="post_add_info">
+					<span class="post_added_by">od:</span>
+					<?php echo datetime($event->event_from, FALSE).' '.time_withou_seconds(datetime($event->event_from, TRUE)); ?>
+					<span class="post_added_by">do:</span>
+					<?php echo datetime($event->event_to, FALSE).' '.time_withou_seconds(datetime($event->event_to	, TRUE)); ?>
+					<span class="post_added_by">vytvorené:</span>
+					<?php echo datetime($event->event_created, FALSE).' '.time_withou_seconds(datetime($event->event_created, TRUE)); ?>
+				<br /> <br /> <p class="separator"></p>
+				</div>
+			</p>
+			<?php
+			}
+		}
 		
 	?>
 </div>
