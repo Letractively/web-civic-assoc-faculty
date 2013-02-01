751177e3af3bc1774d1273f60b72fac57a759da9
psychopepsi3@gmail.com <psychopepsi3@gmail.com@0f6ebd93-bc43-3e5c-799d-1a7754f0039d>
2013-02-01 14:44:47 +0000

    Matej: fixnutie confliktu

--- trunk/application/helpers/project_categories_helper.php
+++ trunk/application/helpers/project_categories_helper.php
@@ -25,27 +25,3 @@ function updateProjCatDetailData($projects)
 	return $result;
 }
 
-/*
- * updateProjectDetailData
- * 
- * Funkcia premapuje stlpec project_active v gride s projektami na textovu podobu
- * 0 zmeni na uzavrety projekt, a 1 zmeni na prebiehajuci projekt
- * 
- * @param projects Array of objects projektov
- * 
- */
-function updateProjectDetailData($projects)
-{	
-	$CI =& get_instance();
-
-	$pStates = array(0 => $CI->lang->line('label_closed'), 1 => $CI->lang->line('label_active'));
-	
-	$result = array();
-	foreach ($projects->result() as $project)
-	{
-		$arr = get_object_vars($project);
-		$arr['project_active'] = in_array($arr['project_active'],array(0,1)) ? $pStates[$arr['project_active']] : '-';
-		$result[] = $arr;
-	}
-	return $result;
-}
