fe729df1e385f75f69ed46ae1426fdf06ff0c99c
psychopepsi3@gmail.com <psychopepsi3@gmail.com@0f6ebd93-bc43-3e5c-799d-1a7754f0039d>
2013-02-04 02:12:33 +0000

    Matej: payment - FIXED, ostava posledny bug

--- trunk/application/views/payments_add_view.php
+++ trunk/application/views/payments_add_view.php
@@ -116,18 +116,13 @@
                                                 $lp = $this->selecter->get_payments_lastpaid($userID);
                             
                               
-						/*if( $lp->payment_paid_sum < $lp->payment_total_sum)
-							redirect(base_url ());*/
 						if( date("Y-m-d", time() - (365 * 86400)) <=  $userActivationDate[0])
 						{
-							//if( $lp->payment_paid_sum >= $lp->payment_total_sum )
-							//{
 								echo form_input(array('name' => 'payment', 'value' => $this->lang->line('payment_type_voluntary'),'disabled'=>'disabled'));
 								echo form_hidden('payment_type', 2);
 								echo '<div class="inputitem"> <span class="label">'.$this->lang->line('label_total_sum').':</span>';
 								echo form_input(array('name' => 'total_sum', 'type' => 'text', 'class' => 'input_data_date'),  set_value('total_sum',1));
 								echo '<span class="label"> €</span> </div>';
-							//}
 						}
 						else if( date("Y-m-d", time() - (365 * 86400)) >  $userActivationDate[0])
 						{
